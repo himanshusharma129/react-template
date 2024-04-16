464239aa8bdce6ef3670b5bfbf9209e588be423b
himanshusharma129 <0hspandit0@gmail.com>
2024-04-16 17:49:06 +0530

    init template

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app main component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const rootElement = screen.getByTestId('app-home');
+  expect(rootElement).toBeInTheDocument();
 });
